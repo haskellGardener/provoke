3e2b6d34991729c69226798fb0a878e0646448a2
Robert E. Lee <robert@cyntrum.com>
2017-09-04 10:56:03 -0500

    reorganized the shelly folder

---
+++ shelly/test/src/TestMain.hs
@@ -0,0 +1,30 @@
+
+module Main where
+
+import ReadFileSpec
+import WhichSpec
+import WriteSpec
+import MoveSpec
+import RmSpec
+import FindSpec
+import EnvSpec
+import FailureSpec
+import CopySpec
+import LiftedSpec
+import RunSpec
+
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+    readFileSpec
+    whichSpec
+    writeSpec
+    moveSpec
+    rmSpec
+    findSpec
+    envSpec
+    failureSpec
+    copySpec
+    liftedSpec
+    runSpec
